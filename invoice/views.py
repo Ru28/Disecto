69f33a0f24924cbd43b4ebaeec79bbcf81652380
Rupesh Virani <rupeshvirani01@gmail.com>
2022-05-26 00:45:54 +0530

    InvoiceListView

--- invoice/views.py
+++ invoice/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views import View
+from .models import Invoice
+
+class InvoiceListView(View):
+    def get(self,*args,**kwargs):
+        invoices = Invoice.objects.all()
+        context = {
+            "invoices":invoices,
+        }
+
+        return render(self.request,'invoice/invoice-list.html',context)
